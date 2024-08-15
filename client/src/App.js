79e010b3ced454d231359a794269c5c418f80476
Srishti S <srishti@Srishtis-MacBook-Air.local>
2024-08-16 00:30:11 +0530

    outfake homepage

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+
+function App() {
+
+  return (
+    <div className="App">
+      <ul className="list">
+          <p>Home Test</p>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
