80e076c736aec9485d72a00bb4550a35f2d06a5b
Srishti S <srishti@Srishtis-MacBook-Air.local>
2024-09-01 11:05:27 +0100

    cyber leaks ui

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 var axios = require('axios');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+
 const app = express();
 const PORT = 5000;
 app.listen(PORT, (error) =>{
@@ -10,11 +10,6 @@ app.listen(PORT, (error) =>{
         console.log("Error occurred, server can't start", error);
     }
 );
-
-app.use('/api', createProxyMiddleware({ 
-    target: 'http://localhost:3000',  // Backend server
-    changeOrigin: true
-}));
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-origin", "*")
