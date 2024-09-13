fd7bf3218235dfe04f8153199f19427cafe4010f
Srishti S <srishti@Srishtis-MacBook-Air.local>
2024-09-13 09:43:27 +0530

    test node react

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === 'production') {
 
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`app running on port ${PORT}`)
 });
