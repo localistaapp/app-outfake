c09d03f4117faa7463260373b71614ca22a035df
Srishti S <srishti@Srishtis-MacBook-Air.local>
2024-09-13 08:56:54 +0530

    test node react

---
+++ routes/productRoutes.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const Product = mongoose.model('products');
+
+module.exports = (app) => {
+
+  app.get(`/api/product`, async (req, res) => {
+    return res.status(200).send([{name: "test product", description: "sample product"}]);
+  });
+
+  app.post(`/api/product`, async (req, res) => {
+    let product = await Product.create(req.body);
+    return res.status(201).send({
+      error: false,
+      product
+    })
+  })
+
+  app.put(`/api/product/:id`, async (req, res) => {
+    const {id} = req.params;
+
+    let product = await Product.findByIdAndUpdate(id, req.body);
+
+    return res.status(202).send({
+      error: false,
+      product
+    })
+
+  });
+
+  app.delete(`/api/product/:id`, async (req, res) => {
+    const {id} = req.params;
+
+    let product = await Product.findByIdAndDelete(id);
+
+    return res.status(202).send({
+      error: false,
+      product
+    })
+
+  })
+  
+}
